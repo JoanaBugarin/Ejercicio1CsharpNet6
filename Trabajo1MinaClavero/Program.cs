e814ed9081a65062908dcd9a374c6a6234596bb2
JoanaBugarin <wooli3000@gmail.com>
2022-05-15 22:50:03 -0300

    Agregar archivos de proyecto.

---
+++ Trabajo1MinaClavero/Program.cs
@@ -0,0 +1,18 @@
+﻿string respuesta = "S";
+while (respuesta.ToUpper() == "S")
+{
+    Console.Clear();
+    Console.Write("Por favor, ingrese su nombre: ");
+    string nombre = Console.ReadLine();
+    Console.WriteLine($"¡Hola {nombre}!");
+    Console.Write("¿Deseas continuar? (S/N): ");
+    respuesta = Console.ReadLine();
+}
+if (respuesta.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+}
+else
+{
+    Console.WriteLine("Opcion no valida");
+}
